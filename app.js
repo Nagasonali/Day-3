ba90492d7a127a405dd0cb9a072f9bb31ec5dd95
Nagasonali <167844487+Nagasonali@users.noreply.github.com>
2024-11-01 13:39:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+function myname(){
+    console.log("Sonali");
+}
+function login(event){
+    event.preventDefault();
+    let mail=document.getElementById("mail").value;
+    let pass=document.getElementById("pass").value;
+    if(mail==="sonali@gmail" && pass==="1234")
+    {
+        alert("Successfully Called");
+        API();
+    }
+    else
+        alert("login not done!");
+}
+async function API()
+{
+    console.log("API called");
+    try{
+        const response= await fetch("https://671b71052c842d92c37fece8.mockapi.io/webworkshop/products");
+        console.log(await response.json());
+    }
+    catch(error){
+        console.error(error);
+    }
+}
